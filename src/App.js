e56a8845ff6cd2fc4ffe33da3243ae29a8be9278
Deepanshu <djain1309@gmail.com>
2023-05-04 17:13:56 -0400

    its working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [allhtml, setAll] = useState("");
+  const [content, setContent] = useState('');
+
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:5000/').then(response => {
+      setContent(response.data);
+    }).catch(err => {
+      console.log(err);
+    })
+  }, [])
+
+
+  const callHandler = () => {
+    const data = {
+      'first' : "Deep",
+      'last' : "Jain"
+    };
+    axios.post('http://127.0.0.1:5000/check', {
+      data : data
+    }).then((response) => {
+      console.log(response.data);
+      setAll(response.data)
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <h2>POC</h2>
+        {content}
+      </nav>
+      <button style={{height : "50px", width: "50px", color: "black"}} onClick={callHandler}>CLICK</button>
+      {<div dangerouslySetInnerHTML={{ __html: allhtml }}></div>}
     </div>
   );
 }
